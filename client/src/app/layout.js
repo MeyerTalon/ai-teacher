1c364ef0e2d20fdb2e935a3e475463ffaa127299
Junwoo Kwon <kwonj5549@gmail.com>
2024-06-22 21:48:26 -0700

    ui shit ur boi went absolutely nuts

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,5 @@
+// src/app/layout.js
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -10,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
   );
 }
